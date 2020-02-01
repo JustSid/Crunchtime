2e684c9dca8b0977245e213fc5ba06b2e287f9e5
Sidney Just <justsid@x-plane.com>
2020-02-01 15:37:50 -0800

    Janky rope physics

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -105,3 +105,130 @@ public class PlayerController : MonoBehaviour
         transform.position += Vector3.up * maxDist;
     }
 }
+
+
+/*
+ *     private void FixedUpdate()
+    {
+        Simulate();
+                       
+        // Higher iteration results in stiffer ropes and stable simulation
+        for (int i = 0; i < 80; i++)
+        {
+            ApplyConstraint();
+
+            // Playing around with adjusting collisions at intervals - still stable when iterations are skipped
+            if (i % 2 == 1)
+                AdjustCollisions();
+        }
+    }
+
+    private void Simulate()
+    {
+        // step each node in rope
+        for (int i = 0; i < TotalNodes; i++)
+        {            
+            // derive the velocity from previous frame
+            Vector3 velocity = RopeNodes[i].transform.position - RopeNodes[i].PreviousPosition;
+            RopeNodes[i].PreviousPosition = RopeNodes[i].transform.position;
+
+            // calculate new position
+            Vector3 newPos = RopeNodes[i].transform.position + velocity;
+            newPos += Gravity * Time.fixedDeltaTime;
+            Vector3 direction = RopeNodes[i].transform.position - newPos;
+                        
+            // cast ray towards this position to check for a collision
+            int result = -1;
+            result = Physics2D.CircleCast(RopeNodes[i].transform.position, RopeNodes[i].transform.localScale.x / 2f, -direction.normalized, ContactFilter, RaycastHitBuffer, direction.magnitude);
+
+            if (result > 0)
+            {
+                for (int n = 0; n < result; n++)
+                {                    
+                    if (RaycastHitBuffer[n].collider.gameObject.layer == 9)
+                    {
+                        Vector2 collidercenter = new Vector2(RaycastHitBuffer[n].collider.transform.position.x, RaycastHitBuffer[n].collider.transform.position.y);
+                        Vector2 collisionDirection = RaycastHitBuffer[n].point - collidercenter;
+                        // adjusts the position based on a circle collider
+                        Vector2 hitPos = collidercenter + collisionDirection.normalized * (RaycastHitBuffer[n].collider.transform.localScale.x / 2f + RopeNodes[i].transform.localScale.x / 2f);
+                        newPos = hitPos;
+                        break;              //Just assuming a single collision to simplify the model
+                    }
+                }
+            }
+
+            RopeNodes[i].transform.position = newPos;
+        }
+    }
+    
+    private void AdjustCollisions()
+    {
+        // Loop rope nodes and check if currently colliding
+        for (int i = 0; i < TotalNodes - 1; i++)
+        {
+            RopeNode node = this.RopeNodes[i];
+
+            int result = -1;
+            result = Physics2D.OverlapCircleNonAlloc(node.transform.position, node.transform.localScale.x / 2f, ColliderHitBuffer);
+
+            if (result > 0)
+            {
+                for (int n = 0; n < result; n++)
+                {
+                    if (ColliderHitBuffer[n].gameObject.layer != 8)
+                    {
+                        // Adjust the rope node position to be outside collision
+                        Vector3 collidercenter = ColliderHitBuffer[n].transform.position;
+                        Vector3 collisionDirection = node.transform.position - collidercenter;
+
+                        Vector3 hitPos = collidercenter + collisionDirection.normalized * ((ColliderHitBuffer[n].transform.localScale.x / 2f) + (node.transform.localScale.x / 2f));
+                        node.transform.position = hitPos;
+                        break;
+                    }
+                }
+            }
+        }    
+    }
+
+    private void ApplyConstraint()
+    {
+        // Check if the first node is clamped to the scene or is follwing the mouse
+        if (Node1Lock != Vector2.zero)
+        {
+            RopeNodes[0].transform.position = Node1Lock;
+        }
+        else
+        {
+            RopeNodes[0].transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        for (int i = 0; i < TotalNodes - 1; i++)
+        {
+            RopeNode node1 = this.RopeNodes[i];
+            RopeNode node2 = this.RopeNodes[i + 1];
+
+            // Get the current distance between rope nodes
+            float currentDistance = (node1.transform.position - node2.transform.position).magnitude;
+            float difference = Mathf.Abs(currentDistance - NodeDistance);
+            Vector2 direction = Vector2.zero;
+           
+            // determine what direction we need to adjust our nodes
+            if (currentDistance > NodeDistance)
+            {
+                direction = (node1.transform.position - node2.transform.position).normalized;
+            }
+            else if (currentDistance < NodeDistance)
+            {
+                direction = (node2.transform.position - node1.transform.position).normalized;
+            }
+
+            // calculate the movement vector
+            Vector3 movement = direction * difference;
+
+            // apply correction
+            node1.transform.position -= (movement * 0.5f);
+            node2.transform.position += (movement * 0.5f);
+        }
+    }
+
+ */
---
+++ Assets/Scripts/Game/Wire.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Wire : MonoBehaviour
+{
+    [SerializeField]
+    private List<Plug> plugs = new List<Plug>();
+
+    [SerializeField]
+    private float extraLength = 10.0f;
+
+    private const float SegmentLength = 0.3f;
+    private const float LineWidth = 0.1f;
+
+    Collider[] ColliderHitBuffer = new Collider[1];
+
+    private List<WireSegment> wireSegments = new List<WireSegment>();
+
+    LineRenderer lineRenderer;
+    Vector3[] segmentPositions;
+
+    void Awake()
+    {
+        Assert.IsTrue(plugs.Count == 2);
+
+        Vector3 start = plugs[0].transform.position;
+        Vector3 end = plugs[1].transform.position;
+        Vector3 direction = end - start;
+
+        int totalSegments = Mathf.CeilToInt((extraLength + direction.magnitude) / SegmentLength);
+
+        float adjustedSegmentLength = direction.magnitude / totalSegments;
+
+        direction = direction.normalized;
+
+        for (int i = 0; i < totalSegments; ++i)
+        {
+            WireSegment node = (GameObject.Instantiate(Resources.Load("WireSegment") as GameObject)).GetComponent<WireSegment>();
+            node.transform.position = start;
+            node.previousPosition = start;
+
+            wireSegments.Add(node);
+
+            start += direction * adjustedSegmentLength;
+        }
+
+        lineRenderer = this.GetComponent<LineRenderer>();
+        lineRenderer.startWidth = LineWidth;
+        lineRenderer.endWidth = LineWidth;
+
+        segmentPositions = new Vector3[totalSegments];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawLineSegments();
+    }
+
+    void FixedUpdate()
+    {
+        Simulate();
+
+        for(int i = 0; i < 80; ++i)
+            AdjustConstraint();
+    }
+
+
+    private void DrawLineSegments()
+    {
+        for(int i = 0; i < wireSegments.Count; ++ i)
+        {
+            segmentPositions[i] = new Vector3(wireSegments[i].transform.position.x, wireSegments[i].transform.position.y, 0);
+        }
+
+        lineRenderer.positionCount = segmentPositions.Length;
+        lineRenderer.SetPositions(segmentPositions);
+    }
+
+
+    private void Simulate()
+    {
+        foreach(WireSegment segment in wireSegments)
+        {
+            Vector3 velocity = segment.transform.position - segment.previousPosition;
+            segment.previousPosition = segment.transform.position;
+
+            Vector3 position = segment.transform.position + velocity;
+            position += Vector3.down * 5f * Time.fixedDeltaTime;
+
+            Vector3 direction = segment.transform.position - position;
+
+#if true
+            RaycastHit[] hits = Physics.SphereCastAll(segment.transform.position, SegmentLength * 0.5f, -direction.normalized, direction.magnitude);
+
+            foreach(RaycastHit hit in hits)
+            {
+                if(hit.collider.GetComponent<Plug>())
+                    continue;
+
+                if(hit.collider.gameObject.layer == 8)
+                {
+                    Vector3 center = hit.collider.transform.position;
+                    Vector3 collisionDirection = hit.point - center;
+
+                    position = hit.point;// + collisionDirection.normalized * SegmentLength * 0.5f;
+                    segment.previousPosition = position;
+                    break;
+                }
+            }
+#endif
+
+            segment.transform.position = new Vector3(position.x, position.y, 0);
+        }
+    }
+
+    private void AdjustConstraint()
+    {
+        wireSegments[0].transform.position = plugs[0].transform.position;
+        wireSegments[wireSegments.Count - 1].transform.position = plugs[1].transform.position;
+
+        for(int i = 0; i < wireSegments.Count - 1; ++i)
+        {
+            WireSegment segment1 = wireSegments[i];
+            WireSegment segment2 = wireSegments[i + 1];
+
+            float distance = (segment1.transform.position - segment2.transform.position).magnitude;
+            float difference = Mathf.Abs(distance - SegmentLength);
+
+            Vector2 direction = Vector2.zero;
+
+            if(distance > SegmentLength)
+                direction = (segment1.transform.position - segment2.transform.position).normalized;
+            else if(distance < SegmentLength)
+                direction = (segment2.transform.position - segment1.transform.position).normalized;
+
+            Vector3 movement = direction * difference;
+            segment1.transform.position -= (movement * 0.5f);
+            segment2.transform.position += (movement * 0.5f);
+        }
+    }
+}
