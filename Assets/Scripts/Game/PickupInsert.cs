42c2338a206f6275a163e76bfcfee50f3c0f8be7
Sidney Just <justsid@widerwille.com>
2020-02-01 20:38:57 -0800

    Stupid highlighting of connections

--- Assets/Scripts/Game/PickupInsert.cs
+++ Assets/Scripts/Game/PickupInsert.cs
@@ -10,6 +10,37 @@ public class PickupInsert : MonoBehaviour
     [SerializeField]
     private List<WirePowerAction> powerActions = new List<WirePowerAction>();
 
+    private List<LineRenderer> lineRenderers = new List<LineRenderer>();
+
+    void Awake()
+    {
+        Material material = new Material(Shader.Find("Unlit/VertexColor"));
+
+        foreach (WirePowerAction action in powerActions)
+        {
+            GameObject go = new GameObject();
+
+            LineRenderer renderer = go.AddComponent(typeof(LineRenderer)) as LineRenderer;
+
+            go.transform.SetParent((transform));
+            go.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+
+            Vector3[] segments = new Vector3[2];
+            segments[0] = action.attachmentPoint ? action.attachmentPoint.position : action.transform.position;
+            segments[1] = transform.position;
+
+            renderer.SetPositions(segments);
+
+            renderer.startColor = Color.red;
+            renderer.endColor = Color.red;
+            renderer.startWidth = 0.1f;
+            renderer.endWidth = 0.1f;
+            renderer.material = material;
+
+            lineRenderers.Add(renderer);
+        }
+    }
+
     private void OnDrawGizmos()
     {
         if (collider == null)
@@ -27,11 +58,23 @@ public class PickupInsert : MonoBehaviour
     {
         foreach(WirePowerAction action in powerActions)
             action.OnPowerEnabledInternal();
+
+        foreach (LineRenderer renderer in lineRenderers)
+        {
+            renderer.startColor = Color.green;
+            renderer.endColor = Color.green;
+        }
     }
 
     public void OnUnplugged()
     {
         foreach(WirePowerAction action in powerActions)
             action.OnPowerDisabledInternal();
+
+        foreach(LineRenderer renderer in lineRenderers)
+        {
+            renderer.startColor = Color.white;
+            renderer.endColor = Color.white;
+        }
     }
 }
