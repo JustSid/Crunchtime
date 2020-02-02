c0e350a0f43473ca7ed2a98fb7e166ede9c7a283
Jared <jaredcobaltgray@mail.com>
2020-02-01 22:46:15 -0800

    FILES

--- Assets/Scripts/ReloadScript.cs
+++ Assets/Scripts/ReloadScript.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class ReloadScript : MonoBehaviour
 {
     public GameObject playerPrefab;
+    public GameObject cameraRigPrefab;
     public string[] levelNames;
-    private int currentlevel;
+    private int currentlevel = -1;
     public static ReloadScript inst;
     private SpawnPoint[] spawners;
     private Transform playerTransform;
+    private PeekeyBoi cameraRig;
 
     void Awake()
     {
@@ -20,47 +22,67 @@ public class ReloadScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Respawn();
+        NextLevel();
+        //Respawn();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) ReloadCurrentLevel();
+    }
+
+    public void ReloadCurrentLevel()
+    {
+        SceneManager.LoadScene(levelNames[currentlevel]);
+        //Respawn();
     }
 
     public void NextLevel()
     {
         currentlevel++;
         SceneManager.LoadScene(levelNames[currentlevel]);
-        Respawn();
+        //Respawn();
     }
 
-    public void Respawn()
-    {
-        if (playerTransform)
-        {
-            Destroy(playerTransform);
-        }
-        playerTransform = Instantiate(playerPrefab).transform;
-        spawners = FindObjectsOfType<SpawnPoint>();
-        SpawnPoint spawner = GetClosestSpawner();
-        playerTransform.SetPositionAndRotation(spawner.transform.position, spawner.transform.rotation);
-    }
+    //public void Respawn()
+    //{
+    //    if (playerTransform)
+    //    {
+    //        Destroy(playerTransform);
+    //    }
+    //    if (cameraRig)
+    //    {
+    //        Destroy(cameraRig);
+    //    }
+    //    playerTransform = Instantiate(playerPrefab).transform;
+    //    //spawners = FindObjectsOfType<SpawnPoint>();
+    //    //SpawnPoint spawner = GetClosestSpawner();
+    //    SpawnPoint spawner = FindObjectOfType<SpawnPoint>();
+    //    playerTransform.SetPositionAndRotation(spawner.transform.position, spawner.transform.rotation);
+    //    cameraRig = Instantiate(cameraRigPrefab).GetComponent<PeekeyBoi>();
+    //    cameraRig.followTarget = playerTransform;
+    //    cameraRig.SnapToTarget();
+    //}
 
-    SpawnPoint GetClosestSpawner()
-    {
-        SpawnPoint best = null;
-        float dist = float.MaxValue;
-        foreach (var t in spawners)
-        {
-            if (!t) continue;
-            float temp = Vector3.Distance(playerTransform.position, t.transform.position);
-            if (!best && t.isActive)
-            {
-                best = t;
-                dist = temp;
-            }
-            else if (temp < dist && t.isActive)
-            {
-                best = t;
-                dist = temp;
-            }
-        }
-        return best;
-    }
+    //SpawnPoint GetClosestSpawner()
+    //{
+    //    SpawnPoint best = null;
+    //    float dist = float.MaxValue;
+    //    foreach (var t in spawners)
+    //    {
+    //        if (!t) continue;
+    //        float temp = Vector3.Distance(playerTransform.position, t.transform.position);
+    //        if (!best && t.isActive)
+    //        {
+    //            best = t;
+    //            dist = temp;
+    //        }
+    //        else if (temp < dist && t.isActive)
+    //        {
+    //            best = t;
+    //            dist = temp;
+    //        }
+    //    }
+    //    return best;
+    //}
 }
