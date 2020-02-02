2271a71dedcd0da035eb066a606d636b1bd91ce9
Jared <jaredcobaltgray@mail.com>
2020-02-02 13:43:46 -0800

    Level-5

---
+++ Assets/KillZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillZone : MonoBehaviour
+{
+    public string levelName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if(player)
+        {
+            SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+        }
+    }
+}
